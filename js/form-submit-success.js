4f83b0b83abe84fa2c9909e9ea0d9f1939cb43b2
Seb James <sebjames5@gmail.com>
2018-10-13 13:03:10 +0000

    added enquiry form functionality

--- js/form-submit-success.js
+++ js/form-submit-success.js
@@ -2,3 +2,8 @@ $('#bookking-form').on('submit', function(e){
   $('#booking-submission').modal('show');
   e.preventDefault();
 });
+
+$('#enquiry-form').on('submit', function(e){
+  $('#enquiry-submission').modal('show');
+  e.preventDefault();
+});
