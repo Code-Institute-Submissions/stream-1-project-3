11fc3d7bf4997cbd4f539c197dcde35057d79d0a
Seb James <sebjames5@gmail.com>
2018-10-16 17:40:25 +0000

    fixed form submission js

--- js/form-submit-success.js
+++ js/form-submit-success.js
@@ -1,9 +1,13 @@
-$('#bookking-form').on('submit', function(e){
-  $('#booking-submission').modal('show');
-  e.preventDefault();
-});
-
-$('#enquiry-form').on('submit', function(e){
-  $('#enquiry-submission').modal('show');
-  e.preventDefault();
+$(function() {
+    $('#booking-form').on('submit', function(e){
+    $('#booking-submission').modal('show');
+    $(this).trigger("reset");
+    e.preventDefault();
+  });
+  
+  $('#enquiry-form').on('submit', function(e){
+    $('#enquiry-submission').modal('show');
+    $(this).trigger("reset");
+    e.preventDefault();
+  });
 });
